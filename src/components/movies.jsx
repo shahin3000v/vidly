fad7527a9a6984f6255bdcd3b77de856a83faee2
shahin3000v <shahin.v@gmail.com>
2020-03-24 13:25:46 +0430

    Like component added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,36 +1,26 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
-    list: getMovies()
+    movies: getMovies()
   };
-  formatTable = () => {
-    return this.state.list.map(film => (
-      <tr key={film._id}>
-        <td>{film.title}</td>
-        <td>{film.genre.name}</td>
-        <td>{film.numberInStock}</td>
-        <td>{film.dailyRentalRate}</td>
-        <td>
-          <button
-            onClick={() => {
-              this.handleDelete(film._id);
-            }}
-            className="btn btn-danger btn-sm"
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
-    ));
-  };
+
   handleDelete = id => {
-    const list = this.state.list.filter(film => film._id !== id);
-    this.setState({ list });
+    const movies = this.state.movies.filter(film => film._id !== id);
+    this.setState({ movies });
+  };
+  toggleLikeHandler = film => {
+    let movies = [...this.state.movies];
+    movies = movies.map(mo => {
+      if (mo === film) mo.liked = !mo.liked;
+      return mo;
+    });
+    this.setState({ movies });
   };
   render() {
-    const { length } = this.state.list;
+    const { length } = this.state.movies;
     if (length === 0)
       return (
         <p className="h4 alert-warning mt-3">There is no record in database!</p>
@@ -47,10 +37,36 @@ class Movies extends Component {
               <th scope="col">Genre</th>
               <th scope="col">Stock</th>
               <th>Rate</th>
+              <th>Like</th>
               <th>#</th>
             </tr>
           </thead>
-          <tbody>{this.formatTable()}</tbody>
+          <tbody>
+            {this.state.movies.map(film => (
+              <tr key={film._id}>
+                <td>{film.title}</td>
+                <td>{film.genre.name}</td>
+                <td>{film.numberInStock}</td>
+                <td>{film.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={film.liked}
+                    toggleLike={() => this.toggleLikeHandler(film)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      this.handleDelete(film._id);
+                    }}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </React.Fragment>
     );
